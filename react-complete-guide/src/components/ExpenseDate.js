8ae1939a95b0ebb6213559b20e8878a88b4eeec2
sureshyalagudri <suresh.yalagudri@gmail.com>
2021-05-14 10:17:36 +0530

    Using CARD as component.]

--- react-complete-guide/src/components/ExpenseDate.js
+++ react-complete-guide/src/components/ExpenseDate.js
@@ -1,16 +1,16 @@
 import './ExpenseDate.css';
-import Card from './Card'
+
 function ExpenseDate(props) {
 
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
     return (
-        <Card className="expense-date">
+        <div className="expense-date">
             <div className="expense-date__month">{month}</div>
             <div className="expense-date__year">{day}</div>
             <div className="expense-date__day">{year}</div>
-        </Card>
+        </div>
     )
 };
 
